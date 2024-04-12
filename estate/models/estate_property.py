f01f39850ac3cb3c566024c283a39e255c6b1e9a
Charlot Raveloarison <charlotclarele@gmail.com>
2024-04-12 04:16:55 +0300

    add property type and relation with each property

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,17 +1,27 @@
+# -*- coding: utf-8 -*-
+
 from odoo import fields, models
 
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real estate property"
+    _order = "id desc"
 
+    # Default value for date availability
+
+    def _default_date_availability(self):
+        return fields.Date.context_today(self)
+
+    # Basic types
     name = fields.Char("Title", required=True)
     description = fields.Text("Descirption")
     postcode = fields.Char()
-    date_availability = fields.Date(required=True)
+    date_availability = fields.Date("Available from",
+                                    required=True, default=lambda self: self._default_date_availability(), copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer(required=True)
+    selling_price = fields.Float(readonly=True)
+    bedrooms = fields.Integer(required=True, default=1)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Integer()
@@ -25,3 +35,9 @@ class EstateProperty(models.Model):
             ('east', 'East'),
             ('west', 'West')
         ])
+
+    # Relational fields
+    property_type_id = fields.Many2one(
+        "estate.property.type", string="Property type")
+    user_id = fields.Many2one(
+        "res.users", string="Salesman", default=lambda self: self.env.user)
---
+++ estate/models/estate_property_type.py
@@ -0,0 +1,11 @@
+from odoo import fields, models
+
+
+class EstatePropertyType(models.Model):
+    _name = "estate.property.type"
+    _description = "Estate property type"
+
+    name = fields.Char("Name", required=True)
+    sequence = fields.Integer("Sequence", default=10)
+    property_ids = fields.One2many(
+        "estate.property", "property_type_id", string="Properties")
